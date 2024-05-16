830a286e83e0a2f846b951359c34d6c2f5ef88b6
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-05-16 11:08:56 +0530

    Delete Auction Bot directory

--- Auction Bot/config/config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-
-# Bot token from BotFather
-TOKEN = '7184177849:AAG-bJzGb-gB0kGHXA-H1_XtC13L8hohmPU'
-
-# Bot owner ID
-BOT_OWNER_ID = 6259443940  # Replace with your Telegram user ID
